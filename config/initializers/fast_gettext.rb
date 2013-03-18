b80e6f2cbd00399463ead9ddf87b6e97abbf2ba2
Bryan Kearney <bkearney@redhat.com>
2013-03-18 09:27:20 -0400

    Change the name of the pot file to foreman, and add a french directory for eLobato

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -3,9 +3,9 @@ require 'gettext_i18n_rails'
 
 locale_dir = File.join(File.dirname(__FILE__), '..', '..', 'locale')
 
-FastGettext.add_text_domain 'app', :path => locale_dir, :type => :po
-FastGettext.default_available_locales = ['en','ja'] #all you want to allow
-FastGettext.default_text_domain = 'app'
+FastGettext.add_text_domain 'foreman', :path => locale_dir, :type => :po
+FastGettext.default_available_locales = ['en','ja', 'fr'] #all you want to allow
+FastGettext.default_text_domain = 'foreman'
 
 module FastGettext
         module Translation
